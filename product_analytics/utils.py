232a8a4ea9ce9d3f28be05e509efa0a6ba40900a
Aurum626 <romanshashkin@mail.ru>
2021-05-03 12:01:18 +0400

    add check environment, delete secret_key

--- product_analytics/utils.py
+++ product_analytics/utils.py
@@ -103,51 +103,85 @@ def count_active_clients_for_period(start_date: date, end_date: date,):
             }
 
 
-def revenue_by_brands(start_date: date, end_date:date):
+def revenue_by_brands(start_date: date, end_date: date):
+    """
+    count revenue for every brands ad return dataframe adapted for html
+    :param start_date: date
+    :param end_date: date
+    :return: DataFrame('brand', 'final_price')
+    """
     orders = MarketOrdersProducts.objects.filter(
         productid__marketgroupid__rn_market_product_parameters__name="Бренд").filter(orderid__status=4).filter(
         created_at__range=(start_date, end_date)). \
         values_list('orderid', 'productid', 'price', 'quantity', 'discount',
                     'productid__marketgroupid__rn_market_product_parameters__name',
                     'productid__marketgroupid__rn_market_product_parameters__value')
+    if len(orders) == 0:
+        return pd.DataFrame(columns=['brand', 'final_price']).to_html()
+    else:
+        df = pd.DataFrame(orders, columns=['orderid', 'productid', 'price', 'quantity', 'discount', 'name_brand', 'brand'])
+        df['discount'] = pd.to_numeric(df['discount'])
+        df['brand'] = df['brand'].str.strip()
+        df['brand'] = df['brand'].str.lower()
+        df['brand'] = df['brand'].str.replace(' ', '')
+        df['final_price'] = df.apply(lambda row: row.price * row.quantity * (100 - row.discount), axis=1)
+        brends_revenue = df.drop_duplicates(subset=['brand'])[['brand', 'final_price']].sort_values('final_price', ascending=False)
+        return brends_revenue.to_html()
 
-    df = pd.DataFrame(orders, columns=['orderid', 'productid', 'price', 'quantity', 'discount', 'name_brand', 'brand'])
-    df['discount'] = pd.to_numeric(df['discount'])
-    df['brand'] = df['brand'].str.strip()
-    df['brand'] = df['brand'].str.lower()
-    df['brand'] = df['brand'].str.replace(' ', '')
-    df['final_price'] = df.apply(lambda row: row.price * row.quantity * (100 - row.discount), axis=1)
-    brends_revenue = df.drop_duplicates(subset=['brand'])[['brand', 'final_price']].sort_values('final_price', ascending=False)
-    return brends_revenue.to_html()
+
+def count_revenue_by_type_clients(start_date: date, end_date: date):
+    """count revenue by new and returning clients"""
+    currently_month = start_date.strftime('%Y-%m')
+    all_orders = MarketOrders.objects.filter(status=4).\
+        filter(deleted_at__isnull=True).values_list('orderid', 'phone', 'sum', 'created_at')
+
+    df = pd.DataFrame(all_orders, columns=['orderid', 'phone', 'sum', 'created_at'])
+    df['join_month'] = df.created_at.apply(lambda x: x.strftime('%Y-%m'))
+    df.set_index('phone', inplace=True)
+    df['join_month'] = df.groupby(level=0)['created_at'].min().apply(lambda x:x.strftime('%Y-%m'))
+    df.astype({'sum': 'float'})
+
+    revenue_by_new_clients = df.loc[df['join_month'] == currently_month]['sum'].sum()
+    df['created_at'] = pd.to_datetime(df['created_at'])
+    mask = (df['created_at'] > str(start_date)) & (df['created_at'] <= str(end_date))
+    revenue_by_returning_clients = df.loc[mask & (df['join_month'] != currently_month)].sum()
+    return {'revenue_by_new_clients': float(revenue_by_new_clients),
+            'revenue_by_returning_clients': float(revenue_by_returning_clients['sum'])}
 
 
 def dashboard_report(start_date, period):
     dates = count_currently_and_previous_date(start_date, period)
-    currently_period_revenue = count_revenue_for_period(dates.get('currently_start'), dates.get('currently_end'))
-    previous_period_revenue = count_revenue_for_period(dates.get('previous_start'), dates.get('previous_end'))
-    currently_active_clients = count_active_clients_for_period(dates.get('currently_start'), dates.get('currently_end'))
-    previous_active_clients = count_active_clients_for_period(dates.get('previous_start'), dates.get('previous_end'))
+    # currently_period_revenue = count_revenue_for_period(dates.get('currently_start'), dates.get('currently_end'))
+    # previous_period_revenue = count_revenue_for_period(dates.get('previous_start'), dates.get('previous_end'))
+    # currently_active_clients = count_active_clients_for_period(dates.get('currently_start'), dates.get('currently_end'))
+    # previous_active_clients = count_active_clients_for_period(dates.get('previous_start'), dates.get('previous_end'))
     df_revenue = pd.DataFrame([count_revenue_for_period(dates.get('currently_start'), dates.get('currently_end')),
                        count_revenue_for_period(dates.get('previous_start'), dates.get('previous_end'))],
                       index=['Текущий', 'Предыдущий'])
     df_clients = pd.DataFrame([count_active_clients_for_period(dates.get('currently_start'), dates.get('currently_end')),
                                count_active_clients_for_period(dates.get('previous_start'), dates.get('previous_end'))],
                                index=['Текущий', 'Предыдущий'])
+    df_revenue_by_type_clients = pd.DataFrame([count_revenue_by_type_clients(dates.get('currently_start'), dates.get('currently_end')),
+                                               count_revenue_by_type_clients(dates.get('previous_start'),
+                                                                               dates.get('previous_end'))],
+                                              index=['Выручка текущий период', 'Выручка предыдущий период'])
     return df_revenue.to_html(float_format=lambda x: '%10.2f' % x), \
-           df_clients.to_html(float_format=lambda x: '%10.2f' % x)
+           df_clients.to_html(float_format=lambda x: '%10.2f' % x), \
+           df_revenue_by_type_clients.to_html(float_format=lambda x: '%10.2f' % x)
+
 
         # currently_period_revenue, previous_period_revenue, currently_active_clients, previous_active_clients
 
 
-def main():
-    start_date = date.today()
-    period = 'week'
-    # start_date = date(2021, 3, 10)
-    # end_date = date(2021, 3, 21)
-
-    dashboard_report(start_date, period)
-    pass
-
-
-if __name__ == '__main__':
-    main()
+# def main():
+#     start_date = date.today()
+#     period = 'week'
+start_date = date(2021, 4, 1)
+end_date = date(2021, 5, 1)
+    #
+    # dashboard_report(start_date, period)
+    # pass
+#
+#
+# if __name__ == '__main__':
+#     main()
--- product_analytics/views.py
+++ product_analytics/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import HttpResponse
-from product_analytics.utils import dashboard_report, revenue_by_brands, count_currently_and_previous_date
+from product_analytics.utils import dashboard_report, revenue_by_brands, count_currently_and_previous_date, \
+    count_revenue_by_type_clients
 from product_analytics.cohorts import cohorts
 from datetime import date
 
@@ -8,8 +9,8 @@ from datetime import date
 def dashboard(request):
     dates = count_currently_and_previous_date(date.today(), 'month')
     revenue_brands = revenue_by_brands(dates.get('currently_start'), dates.get('currently_end'))
-    revenue, clients = dashboard_report(date.today(), 'month')
+    revenue, clients, revenue_by_type_clients = dashboard_report(date.today(), 'month')
     cohort = cohorts()
-    html = "<html><body>%s</body></html>" % revenue, clients, revenue_brands, cohort
+    html = "<html><body>%s</body></html>" % revenue, revenue_by_type_clients, clients, revenue_brands, cohort
     return HttpResponse(html)
 
